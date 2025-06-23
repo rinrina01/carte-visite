248ec8526dc8ec9d0a202fb7fcaa697017aef8ed
BalletsPistol <brosstar.france@gmail.com>
2025-06-23 15:04:43 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -50,14 +50,14 @@ END:VCARD
               className="object-cover rounded-full" // Ensures the image fits well
             />
           </div>
-          <h1 className="text-3xl font-bold uppercase text-stone-100">An Tran</h1>
+          <h1 className="text-3xl font-bold uppercase text-stone-100">Frederick Tran</h1>
           <p className="text-sm text-stone-400 mt-2">Ligne Chic - L&apos;élégance intemporelle</p>
         </div>
 
         {/* Contact Information Section */}
         <div className="p-8">
           <div className="mb-6">
-            <h2 className="text-2xl font-bold text-yellow-500">An Tran</h2>
+            <h2 className="text-2xl font-bold text-yellow-500">Frederick Tran</h2>
             <p className="text-stone-400">Architecte d&apos;intérieur designer</p>
           </div>
 
